3ed95c9b3310577da995f47d42b08c183aaa0523
Lukas Sorensen <lsorensen@sportsdigita.com>
2025-05-02 11:54:51 -0500

    format keys with dashes

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ use std::fs;
 use std::path::PathBuf;
 
 static KEYCODES_JSON: &str = include_str!("../data/keycodes.json");
-const NUMBER_OF_COLUMNS: i32 = 6;
+const NUMBER_OF_COLUMNS: i32 = 12;
 const NUMBER_OF_ROWS: i32 = 3;
 const IS_SPLIT: bool = true;
-const NUMBER_OF_THUMB_KEYS: i32 = 3;
+const NUMBER_OF_THUMB_KEYS: i32 = 6;
+const KEY_DISPLAY_CHAR_WIDTH: i32 = 7;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
@@ -122,6 +123,9 @@ fn main() -> Result<()> {
                 let mut keycode_index = 0;
                 let mut row_index = 1;
                 for keycode in reg_exp_layer.find_iter(&inner_str) {
+                    if (keycode_index == 0) {
+                        print_dashes();
+                    }
                     keycode_index = keycode_index + 1;
                     let mut is_end_row = false;
                     let mut is_split_gap = false;
@@ -131,35 +135,46 @@ fn main() -> Result<()> {
                     if is_thumb_row {
                         is_end_row = keycode_index % NUMBER_OF_THUMB_KEYS == 0;
                         is_split_gap =
-                            is_end_row && keycode_index % (NUMBER_OF_THUMB_KEYS * 2) != 0;
+                            !is_end_row && keycode_index % (NUMBER_OF_THUMB_KEYS / 2) == 0;
                     } else {
                         is_end_row = keycode_index % NUMBER_OF_COLUMNS == 0;
                         is_split_gap =
-                            IS_SPLIT && is_end_row && keycode_index % (NUMBER_OF_COLUMNS * 2) != 0;
-                    }
-
-                    // println!("is_end_row {}", is_end_row);
-                    // println!("is_split_gap {}", is_split_gap);
-                    if is_split_gap {
-                        print!("     ")
-                    }
-
-                    if !is_split_gap && is_end_row {
-                        println!("|");
-                        row_index = row_index + 1;
+                            !is_end_row && IS_SPLIT && keycode_index % (NUMBER_OF_COLUMNS / 2) == 0;
                     }
 
                     let keycode_str = keycode.as_str();
-                    let human_readable = get_key_code_human_readable(&keycode_str, &keymap_dict);
-                    print!("{} ", human_readable);
+                    let mut human_readable =
+                        get_key_code_human_readable(&keycode_str, &keymap_dict);
+                    human_readable = create_key_gui(&human_readable);
+                    print!("{}", human_readable);
+
+                    if is_split_gap {
+                        print!("|      ")
+                    }
+
+                    if is_end_row {
+                        println!("|");
+                        print_dashes();
+                        row_index = row_index + 1;
+                    }
                 }
                 println!("");
+                print_dashes();
                 println!("---- LAYER end ----");
             }
         }
     }
 
     Ok(())
+}
+
+fn print_dashes() {
+    let num_of_dashes = ((KEY_DISPLAY_CHAR_WIDTH + 1) * NUMBER_OF_COLUMNS) + 8;
+    let mut dashes = String::new();
+    for _ in 0..num_of_dashes {
+        dashes.push('-');
+    }
+    println!("{}", dashes.to_string());
 }
 
 fn get_key_code_human_readable(keycode: &str, keymap_dictionary: &KeymapDictionary) -> String {
@@ -172,4 +187,25 @@ fn get_key_code_human_readable(keycode: &str, keymap_dictionary: &KeymapDictiona
     } else {
         keycode_str
     }
+}
+
+fn create_key_gui(key_str: &str) -> String {
+    let short_key = key_str
+        .chars()
+        .take(KEY_DISPLAY_CHAR_WIDTH as usize)
+        .collect::<String>();
+    let key_length = short_key.len();
+    let mut s = String::new();
+    let n_spaces = KEY_DISPLAY_CHAR_WIDTH - key_length as i32;
+    let l_spaces = n_spaces / 2;
+    let r_spaces = n_spaces - l_spaces;
+    s.push('|');
+    for _ in 0..l_spaces {
+        s.push(' ')
+    }
+    s.push_str(&short_key);
+    for _ in 0..r_spaces {
+        s.push(' ')
+    }
+    s
 }

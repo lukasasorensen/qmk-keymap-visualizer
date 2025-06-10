d619028a3cdd1c2cf062b889cf5cf82a68d62aa7
Lukas Sorensen <lsorensen@sportsdigita.com>
2025-06-10 09:36:05 -0500

    better gui

--- src/render/gui.rs
+++ src/render/gui.rs
@@ -29,30 +29,39 @@ impl eframe::App for KeymapApp {
             style.visuals.widgets.active.bg_fill = Color32::from_rgb(150, 0, 0);
             style.visuals.widgets.inactive.bg_fill = Color32::from_rgb(200, 200, 200);
             style.visuals.widgets.hovered.bg_fill = Color32::from_rgb(180, 180, 0);
-            style.visuals.widgets.active.fg_stroke = Stroke::new(10.0, Color32::from_rgb(255, 255, 255));
+            style.visuals.widgets.active.fg_stroke =
+                Stroke::new(10.0, Color32::from_rgb(255, 255, 255));
 
             ctx.set_style(style);
             egui::ScrollArea::vertical().show(ui, |ui| {
                 for layers in &self.keymap {
                     for row in layers {
-                        ui.columns(row.len() as usize, |columns| {
-                            for (keycode_index, keycode) in row.iter().enumerate() {
+                        ui.horizontal(|ui| {
+                            for keycode in row {
                                 let human_readable = keycode_util::get_key_code_human_readable(
                                     keycode,
                                     &self.keymap_dict,
                                 );
 
-                                let formatted = format!("\n  {}  \n", human_readable);
+                                // let key_gui = keycode_util::create_key_gui(&human_readable);
+
+                                // let formatted = format!("\n{}\n", key_gui);
+
+                                let formatted = human_readable.chars().take(7).collect::<String>();
+
                                 let rich_text = egui::RichText::new(formatted)
                                     .family(egui::FontFamily::Monospace)
                                     .size(16.0);
 
-                                let button = columns[keycode_index].button(rich_text);
-                                if button.clicked() {
+                                if ui
+                                    .add_sized([80.0, 50.0], egui::Button::new(rich_text))
+                                    .clicked()
+                                {
                                     println!("Clicked: {}", keycode);
-                                }   
+                                }
                             }
                         });
+                        ui.add_space(10.0);
                     }
                 }
             });
--- src/utils/keycode_util.rs
+++ src/utils/keycode_util.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 
 pub type KeymapDictionary = HashMap<String, String>;
 
+const KEY_DISPLAY_CHAR_WIDTH: i32 = 7;
+
 pub fn get_key_code_human_readable(keycode: &str, keymap_dictionary: &KeymapDictionary) -> String {
     let mut keycode_str = keycode.to_string();
     keycode_str = keycode_str.trim().to_string();
@@ -22,4 +24,20 @@ pub fn get_key_code_human_readable(keycode: &str, keymap_dictionary: &KeymapDict
     } else {
         keycode_str
     }
+}
+
+pub fn create_key_gui(key_str: &str) -> String {
+    let key = key_str
+        .chars()
+        .take(KEY_DISPLAY_CHAR_WIDTH as usize)
+        .collect::<String>();
+    let padding = KEY_DISPLAY_CHAR_WIDTH - key.len() as i32;
+    let left_pad = padding / 2;
+    let right_pad = padding - left_pad;
+    format!(
+        "{}{}{}",
+        " ".repeat(left_pad as usize),
+        key,
+        " ".repeat(right_pad as usize)
+    )
 }
